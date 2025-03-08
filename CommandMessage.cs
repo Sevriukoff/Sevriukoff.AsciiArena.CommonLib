eda57872f68393a5dad0f1b20a2cd77c96e90da1
Sevriukoff <igor.sevriukoff@yandex.ru>
2025-03-08 13:25:56 +0700

    Added minimal Player and CommandMessage classes

---
+++ CommandMessage.cs
@@ -0,0 +1,7 @@
+namespace Sevriukoff.AsciiArena.CommonLib;
+
+public class CommandMessage
+{
+    public string Command { get; set; }
+    public string Direction { get; set; }
+}
---
+++ Player.cs
@@ -0,0 +1,8 @@
+namespace Sevriukoff.AsciiArena.CommonLib;
+
+public class Player
+{
+    public Guid Id { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+}
